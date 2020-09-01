63e241ed938df66c5920d8393ebe407bbbeed641
Lotte <lotteravn@live.dk>
2020-09-01 11:29:43 +0200

    1. opgave grader

---
+++ js15.js
@@ -0,0 +1,14 @@
+"use strict";
+/* print converted number from
+   fahrenheit to celsius */
+let fahr;
+let celsius;
+
+
+
+
+fahr = window.prompt('Enter grader i fahrenheit')
+
+celsius = 5 / 9 * (fahr - 32);
+
+console.log(celsius);
